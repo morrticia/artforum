1d7fac23eb29002f7bac97468e50be73866e2de4
mrticia <nino.darsavelidze.3@iliauni.edu.ge>
2020-08-10 14:27:39 +0400

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authentication, identity
+
+from resources.item import Items, Item
+from resources.user import Account
+
+app = Flask(__name__)
+app.secret_key = "TopSecret"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+jwt = JWT(app, authentication, identity)
+api = Api(app)
+
+api.add_resource(Item, "/artwork/<string:name>")
+api.add_resource(Items, "/artworks")
+api.add_resource(Account, "/registration")
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+if __name__ == "__main__":
+    from db import db
+    db.init_app(app)
+
+    app.run(port=5000, debug=True)
+
+

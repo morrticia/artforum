cf43fd020068e0b55776f21f854239ee5a39d94c
mrticia <nino.darsavelidze.3@iliauni.edu.ge>
2020-08-19 21:30:50 +0400

    Fixing heroku eror

--- app.py
+++ app.py
@@ -27,10 +27,10 @@ api.add_resource(Item, "/artwork/<string:name>")
 api.add_resource(Items, "/artworks")
 api.add_resource(Account, "/registration")
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
+#
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 if __name__ == "__main__":
     from db import db
